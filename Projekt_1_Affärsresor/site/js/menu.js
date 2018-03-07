9b9c94f9bae620d907aeceb37bdbec510596091e
itggot-timothy-nilsson <timothy.nilsson@itggot.se>
2018-03-07 22:09:23 +0100

    Fixed hamburger menu
    Fixat hamburgar meny i html

---
+++ Projekt_1_Affärsresor/site/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".menu");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".fade-layer");
+    element.classList.toggle("fade");
+}
